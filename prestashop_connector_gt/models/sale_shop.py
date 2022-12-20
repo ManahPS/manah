e947ebfbeff2f40fe3232c63246feb3e19adf3d4
LFer <lferreira@proyectasoft.com>
2022-12-20 15:06:08 -0300

    removed ipdb

--- prestashop_connector_gt/models/sale_shop.py
+++ prestashop_connector_gt/models/sale_shop.py
@@ -15,8 +15,6 @@
 #    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-import ipdb
-
 from odoo import api, fields, models, _
 from datetime import timedelta, datetime, date, time
 from odoo.exceptions import UserError, ValidationError
